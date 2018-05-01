ffaf2190791ca8c811c9c43273e5f6513c599e1b
saques <msaques@itba.edu.ar>
2018-05-01 16:51:44 -0300

    additional check

--- problem/src/main/java/ar/edu/itba/sia/chainreaction/problem/ChainReactionLeftVsOpenHeuristic.java
+++ problem/src/main/java/ar/edu/itba/sia/chainreaction/problem/ChainReactionLeftVsOpenHeuristic.java
@@ -8,15 +8,15 @@ import java.util.List;
 /**
  * Created by alejosaques on 1/5/18.
  */
-public class TestH implements Heuristic<ChainReactionState> {
-
+public class ChainReactionLeftVsOpenHeuristic implements Heuristic<ChainReactionState> {
 
   @Override
   public double getValue(ChainReactionState c) {
     List<Pair<Integer, Integer>> neighbours = c.getNeighbours();
     int open = neighbours.size();
+    if(open == 0 && c.getLeft() != 0)
+      return Double.POSITIVE_INFINITY;
     return c.getLeft() - 1.0 / open;
   }
-
 
 }
